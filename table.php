701b490c31d0a39ecf5a9227403983ba6d29c82e
Amir <amir.metmati@gmail.com>
2026-01-06 12:20:04 +0100

    table php

---
+++ table.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simulation</title>
+</head>
+<body>
+    <h1>Simulation de combat</h1>
+    <h3>Simulez votre combat Pokémon et voyez quelle est la meilleure comboinaison</h3>
+    <div class="select">
+        <div>
+            
+        </div>
+        <div>
+            
+        </div>
+    </div>
+</body>
+</html>
