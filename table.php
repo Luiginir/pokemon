b6cc79343d1ea78142339abd0f6daef19170728d
Amir <amir.metmati@gmail.com>
2026-01-07 11:17:35 +0100

    jsp

--- table.php
+++ table.php
@@ -9,11 +9,26 @@
     <h1>Simulation de combat</h1>
     <h3>Simulez votre combat Pokémon et voyez quelle est la meilleure comboinaison</h3>
     <div class="select">
-        <div>
-            
+        <div class="deck">
+            <h4>Equipe 1</h4>
+            <?php 
+                // Load Pokemon data from JSON file
+                $json_file = 'assets/data/pokemons.json';
+                $json_data = file_get_contents($json_file);
+                $pokemons = json_decode($json_data, true);
+                
+                for ($i = 1; $i <= 9; $i++) {
+                    echo '<label for="pokemon' . $i . '">Selectionner un pokemon</label>';
+                    echo '<select name="pokemon' . $i . '" id="pokemon' . $i . '">';
+                    foreach ($pokemons as $pokemon) {
+                        echo "<option value='" . $pokemon['#'] . "'>" . $pokemon['Name'] . "</option>";
+                    }
+                    echo '</select>';
+                }
+            ?>
         </div>
-        <div>
-            
+        <div class="deck">
+            <h4>Equipe 2</h4>
         </div>
     </div>
 </body>
